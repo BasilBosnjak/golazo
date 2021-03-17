c3c8b22e6744bb4da1c28accde8010a6c4997106
BasilBosnjak <basil.bosnjak@stu.ibu.edu.ba>
2021-03-17 14:35:51 +0100

    DAO Creation

--- API/DAO/BaseDao.class.php
+++ API/DAO/BaseDao.class.php
@@ -1,23 +1,35 @@
 <?php
 class BaseDao {
-    public function __construct(){
 
+    public function __construct(){
+    $servername = "localhost";
+    $username = "root";
+    $password = "12345678";
+
+    try {
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+}   catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
     }
 
     public function insert(){
-
+        /*lagano*/
     }
 
     public function update(){
-
+        /*lagano*/
     }
 
     public function query(){
-
+        /*lagano*/
     }
 
     public function query_unique(){
-
+        /*lagano*/
     }
     
         
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
+require_once dirname(__FILE__)."/DAO/BaseDao.class.php";
+$lendric = new BaseDao();
 echo "test test.";
 ?>
